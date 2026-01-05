5715fc6d822e71c6ab1b245bfb5a324cf7108e53
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-05 20:12:01 +0000

    add the most basic example of a shared read value

---
+++ bin/stocks_00_shared_read_value_external.ml
@@ -0,0 +1,21 @@
+open Core
+open Par_samples
+
+let is_weird_stock_par (par : Parallel.t) stock =
+  let is_penny_stock stock = Float.(Stock.price stock < 1.0) in
+  let is_five_digit_stock stock = Float.(Stock.price stock >= 10000.0) in
+  let #(is_penny, is_five_digit) =
+    Parallel.fork_join2 par
+      (fun _par -> is_penny_stock stock)
+      (fun _par -> is_five_digit_stock stock)
+  in
+  (is_penny, is_five_digit)
+;;
+
+let stock1 = Stock.create ~price:0.07
+
+let run par = is_weird_stock_par par stock1
+
+let () =
+  let (is_penny, is_five_digit) = Parallel_utils.run_one_test ~f:run in
+  Printf.printf "result: %b %b\n" is_penny is_five_digit
