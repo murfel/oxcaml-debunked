ad9e30af5735b83e8311006c3cc7b6a62dcab03f
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-05 23:07:39 +0000

    add compiler output to sample

--- bin/stocks_00_shared_read_external.ml
+++ bin/stocks_00_shared_read_external.ml
@@ -1,6 +1,15 @@
 open Core
 open Par_samples
 
+(* File "bin/stocks_00_shared_read_external.ml", line 10, characters 19-38: *)
+(* 10 |       (fun _par -> is_five_digit_stock stock) *)
+(*                        ^^^^^^^^^^^^^^^^^^^ *)
+(* Error: The value is_five_digit_stock is nonportable *)
+(*       because it closes over the value Stock.price (at File "bin/stocks_00_shared_read_external.ml", line 6, characters 41-52) *)
+(*       which is nonportable. *)
+(*       However, the highlighted value is_five_digit_stock is expected to be portable *)
+(*       because it is used inside a function which is expected to be portable. *)
+
 let calc_weird_stock_tuple_par (par : Parallel.t) stock =
   let is_penny_stock stock = Float.(Stock.price stock < 1.0) in
   let is_five_digit_stock stock = Float.(Stock.price stock >= 10000.0) in
