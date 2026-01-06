fb552564db05ab4381dc271b81b0be4be15ce8ad
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-06 14:58:25 +0000

    rework the data race example (profitable)

--- bin/profitable.ml
+++ bin/profitable.ml
@@ -1,9 +1,25 @@
 open Core
 
-(* Given a list of prices for a security and a threshold price,
-     count the number of days when price > threshold.
-     Speed up the computation by splitting the list in two,
-     and counting on two domains (threads). *)
+(* Given a list of daily closing prices for a security and and threshold price, *)
+(* count the number of days when price > threshold. *)
+
+(* Sequential version *)
+
+let prices = [3.5; 2.1; 3.3; 5.7; 1.8]
+let threshold = 2.0
+
+let count_profitable_days prices threshold =
+  List.count prices ~f:(fun price -> Float.(price > threshold))
+
+let () =
+  let result = count_profitable_days prices threshold in
+  Printf.printf "result (sequential): %d\n" result (* result: 4 *)
+
+(* Speed up the computation by parallelizing by data: *)
+(* split the list in two, and count each part on its own domain (thread). *)
+
+(* Incorrect parallel version (data race on `counter`) (does not compile) *)
+
 (* let count_profitable_days_par (par : Parallel.t) prices threshold = *)
 (*  let count = ref 0 in *)
 (*  let (left, _right) = List.split_n prices (List.length prices / 2) in *)
@@ -15,36 +31,25 @@ open Core
 (*  let _ = *)
 (*    Parallel.fork_join2 par *)
 (*      (fun _par -> count_profitable_days left) *)
-(* (*      Replace the right part of the array computation with an empty computation. *)
-(*        And replace right with _right above. *)
-(*        It will compile, because the compiler will infer that count is only modified in one domain. *)
-(* *) *)
-(* (*      (fun _par -> ()) *) *)
 (*      (fun _par -> count_profitable_days right) *)
 (*  in *)
 (*  !count *)
 (* ;; *)
 
-(* let statistics = ref 0 *)
-
+(* Correct parallel version *)
 
 let count_profitable_days_par (par : Parallel.t) prices threshold =
   let (left, right) = List.split_n prices (List.length prices / 2) in
-  let is_profitable price = Float.(price > threshold) in
-  let count_profitable_days lst = List.count lst ~f:is_profitable in
   let #(left_count, right_count) =
     Parallel.fork_join2 par
-      (fun _par -> count_profitable_days left)
-      (fun _par -> count_profitable_days right)
+      (fun _par -> count_profitable_days left threshold)
+      (fun _par -> count_profitable_days right threshold)
   in
   left_count + right_count
 ;;
 
-
-let run par = count_profitable_days_par par [3.5; 2.1; 3.3; 5.7; 1.8] 2.0
+let run par = count_profitable_days_par par prices threshold
 
 let () =
   let result = Parallel_utils.run_one_test ~f:run in
-  Printf.printf "result: %d\n" result
-
-(* let () = print_endline "Hello, World!" *)
+  Printf.printf "result (parallel): %d\n" result
