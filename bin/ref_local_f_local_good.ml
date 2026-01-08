73f51f0434da2b7fea97ad839eb35eb0abbe9447
Natasha Murashkina <murfel@users.noreply.github.com>
2026-01-08 17:44:10 +0000

    add global/local ref examples

--- bin/ref_local_f_local_good.ml
+++ bin/ref_local_f_local_good.ml
@@ -1,18 +1,13 @@
-(*  Does not compile. *)
-
-(* 7 | let f () = x := !x + 1 *)
-(*               ^ *)
-(* Error: Unbound value x *)
-
-let f () = x := !x + 1
+(*  Compiles. *)
 
 let run (par : Parallel.t) =
   let x = ref 0 in
+  let f () = x := !x + 1 in
   let _ = Parallel.fork_join2 par (fun _par -> f ()) (fun _par -> 42) in
   !x
 ;;
 
 let () =
   let result = Parallel_utils.run_one_test ~f:run in
-  Printf.printf "result: %d\n" result
+  Printf.printf "result: %d\n" result (* result: 1 *)
 ;;
